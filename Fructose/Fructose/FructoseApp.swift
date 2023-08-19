6e03b9825d72daaec5db7db771790d27608c51d9
Arsham1024 <Arsham512@gmail.com>
2023-08-18 20:18:44 -0700

    create the xcode project

---
+++ Fructose/Fructose/FructoseApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructoseApp.swift
+//  Fructose
+//
+//  Created by Arsham Mehrani on 8/18/23.
+//
+
+import SwiftUI
+
+@main
+struct FructoseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
